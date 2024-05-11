c1369d5f84210a1d339e78dbc246377ee3518b35
Mati <sh4cun@gmail.com>
2024-05-11 18:52:20 +0200

    empty projects added to structure

---
+++ lunch-service/src/main/java/com/wheretolunch/lunchservice/LunchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wheretolunch.lunchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchServiceApplication.class, args);
+	}
+
+}
